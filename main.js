c70e214b50b0ee4f90347ff74446f9824cfb83e3
Ailana Askar <ailana.askar@nu.edu.kz>
2020-08-06 20:37:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+var pg = require('pg');
+
+
+var connectionString = {
+    host: 'localhost',
+    port: 5432,
+    database: 'chart',
+    user: 'ailana',
+    password: 'q'
+}
+// var connectionString = "postgres://ailana:q@localhost/ip:5432/chart";
+var pgClient = new pg.Client(connectionString);
+pgClient.connect();
+var id_query = pgClient.query("SELECT id from chart");
+var value_query = pgClient.query("SELECT value from chart");
+pgClient.end();
+
+//somehow connect with the database
+
+var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
